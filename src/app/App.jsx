52df02817d08bf4eea3e969ac75cdeb213b96e7a
Karina Lopez <lopezka1@karinas-mbp.lan>
2020-06-24 21:46:43 -0400

    changed classes to functional components and use react hooks

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,14 @@
-import react from 'react';
-import HomeButton from '../components/HomeButton';
+import React, {useState} from 'react';
+import ResumeButton from '../components/ResumeButton.jsx';
 
-export default class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            hide=false,
-        }
-    }
-    render(){
-        return(
-            <HomeButton redirect="resume" onclick={this.state.hide}/>
-        )
-    }
+function App(){
+    const [hide,setHide]=useState(false);
+    return( <div>
+          Click me!
+    <ResumeButton onclick={()=>setHide(!hide)}/>
+        </div>)
+       
+    
 }
+export default App;
+
---
+++ src/components/ResumeButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ResumeButton (){
+    return(
+        <div>
+       <button>Resume</button>
+        </div>
+    );  
+}
+export default ResumeButton;

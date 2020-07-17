3672f150ea5c8493a0847a0b9ed26805864cfad5
Karina Lopez <lopezka1@Karinas-MacBook-Pro.local>
2020-07-17 18:03:56 -0400

    added code to update quote after fetch request

--- src/components/QuoteReel.jsx
+++ src/components/QuoteReel.jsx
@@ -1,8 +1,17 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 
 function QuoteReel(){
+    const [data, setData] = useState({quote:[]});
+    
+    
+    useEffect(()=>{
+        const fetchData = fetch('').then(response=>response.json()).then(result=>{
+        setData=result.value[0];
+    });
+    })
+    
 return(
-            <div className='quoteBlock'><div className='apiQuote'><p className='quote'>Quote Here</p><p className='author'>Author</p></div></div>
+            <div className='quoteBlock'><div className='apiQuote'><p className='quote'>{data.quote}</p><p className='author'>{data.quote}</p></div></div>
 
 )
 }

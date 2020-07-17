43e8ac9b1b0dec13d6047c30c45584a6e785023c
Karina Lopez <lopezka1@karinas-mbp.lan>
2020-07-17 19:57:50 -0400

    quote api working

--- src/components/QuoteReel.jsx
+++ src/components/QuoteReel.jsx
@@ -3,15 +3,21 @@ import React,{useState, useEffect} from 'react';
 function QuoteReel(){
     const [data, setData] = useState({quote:[]});
     
-    
-    useEffect(()=>{
-        const fetchData = fetch('').then(response=>response.json()).then(result=>{
-        setData=result.value[0];
+    const fetchData = fetch('https://type.fit/api/quotes').then(response=>response.json()).then(result=>{
+       debugger;
+        setData(result[0]);
+        console.log(data);
+
     });
-    })
+    /*
+    useEffect(()=>{
+        const fetchData = fetch('https://type.fit/api/quotes').then(response=>response.json()).then(result=>{
+        setData=result[0];
+    });
+    })*/
     
 return(
-            <div className='quoteBlock'><div className='apiQuote'><p className='quote'>{data.quote}</p><p className='author'>{data.quote}</p></div></div>
+            <div className='quoteBlock'><div className='apiQuote'><p className='quote'>{data.text}</p><p className='author'>{data.author}</p></div></div>
 
 )
 }

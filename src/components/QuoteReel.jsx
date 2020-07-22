e376c9e7e8c58df30c87e14162c2dc3fb7f36fec
Karina Lopez <lopezka1@karinas-mbp.lan>
2020-07-21 21:45:56 -0400

    stopped endless updates with fetch and made responsive web changes

--- src/components/QuoteReel.jsx
+++ src/components/QuoteReel.jsx
@@ -1,20 +1,12 @@
 import React,{useState, useEffect} from 'react';
-import {GenerateRandom} from './RandomNumberGen.js';
-
 function QuoteReel(){
     const [data, setData] = useState({quote:[]});
-    /*
-    const fetchData = fetch('https://type.fit/api/quotes').then(response=>response.json()).then(result=>{
-       debugger;
-        setData(result[0]);
-
-    });*/
+  
     useEffect(()=>{
             fetch('https://type.fit/api/quotes').then(response=>response.json()).then(result=>{
-           var randomQuoteIndex =  GenerateRandom(result);
-            setData(result[randomQuoteIndex]);
+            setData(result[0]);
             })
-    });
+    },[]);
     
 return(
             <div className='quoteBlock'><div className='apiQuote'><p className='quote'>{data.text}</p><p className='author'>{data.author}</p></div></div>

592ae3d0669e1235fa3a3a1fa7ab5c0c61ad9b00
Karina Lopez <karinalopez@karinas-mbp.lan>
2021-03-24 22:15:27 -0400

    random number generate for quote to change on load

--- src/components/QuoteReel.jsx
+++ src/components/QuoteReel.jsx
@@ -1,10 +1,12 @@
 import React,{useState, useEffect} from 'react';
 function QuoteReel(){
     const [data, setData] = useState({quote:[]});
-  
+
     useEffect(()=>{
             fetch('https://type.fit/api/quotes').then(response=>response.json()).then(result=>{
-            setData(result[0]);
+            let results= result.length;
+            let quoteInt = getRandomInt(results);
+            setData(result[quoteInt]);
             })
     },[]);
     
@@ -12,5 +14,10 @@ return(
             <div className='quoteBlock'><div className='apiQuote'><p className='quote'>{data.text}</p><p className='author'>{data.author}</p></div></div>
 
 )
+}
+function getRandomInt(max){
+    let rand = Math.random();
+
+    return Math.floor(rand* max);
 }
 export default QuoteReel;

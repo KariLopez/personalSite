97951390e4650e80ce0a6956155d554107083cbb
Karina Lopez <karinalopez@karinas-mbp.lan>
2021-04-19 22:09:44 -0400

    better menu display

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,13 @@
 import React,{useState} from 'react';
+import menu from '../styling/menu.css';
+
 
 function Menu(){
     const [show, seeMenu] = useState(false);
 
-    let menuitems=<div></div>
+    let menuitems=<div></div>;
     if(show){
-        menuitems= <div><div><a href='index.html'>  Home</a></div><div><a href='work.html'>Work</a></div><div><a href='about.html'>About Me</a></div><div><a href="https://drive.google.com/file/d/1YGtzrhmGB83dFQKcadWHazXDCvX3Zhj3/view?usp=sharing" target="_blank">Resume</a></div></div>;         
+        menuitems= <div className='menuItems'><div><a href='index.html'>  Home</a></div><div><a href='work.html'>Work</a></div><div><a href='about.html'>About Me</a></div><div><a href="https://drive.google.com/file/d/1YGtzrhmGB83dFQKcadWHazXDCvX3Zhj3/view?usp=sharing" target="_blank">Resume</a></div></div>;         
     }
     return(
         <div className='menu'>

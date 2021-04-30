27e132e759261e316054816af9b35ed5c61e8aff
Karina Lopez <karinalopez@karinas-mbp.lan>
2021-04-30 14:34:40 -0400

    update resume link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,7 +7,9 @@ function Menu(){
 
     let menuitems=<div></div>;
     if(show){
-        menuitems= <div className='menuItems'><div><a href='index.html'>  Home</a></div>{/*<div><a href='work.html'>Work</a></div>*/}<div><a href='about.html'>About</a></div><div><a href="https://docs.google.com/document/d/1pPrCUpt-EN96BcgvytCy80R3YwMfYDwpuMcof9RQeWg/edit?usp=sharing" target="_blank">Resume</a></div></div>;         
+        menuitems= <div className='menuItems'><div><a href='index.html'>  Home</a></div>{/*<div><a href='work.html'>Work</a></div>*/}<div><a href='about.html'>About</a></div><div>
+            <a href="https://docs.google.com/document/d/e/2PACX-1vSk0pO2oLehnI5iGtC8z84tjefGfgPyezzKvNwN3YvKTfmCXiyJ-dDFZuP2qvAklrP9wQwNdrYqv6ME/pub" target="_blank">Resume</a>
+            </div></div>;         
     }
     return(
         <div className='menu'>

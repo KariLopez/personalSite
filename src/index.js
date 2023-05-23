f6072b3eb48c50f7d2f62ee1064b0bba018ecb44
KariLopez <karinalopez91@gmail.com>
2023-05-23 14:01:37 -0400

    remove dead code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App.jsx';
-import NewApp from './NavPages/NewApp.jsx';
+import NewApp from './NavPages/App.jsx';
 
 ReactDOM.render(
   <NewApp />,
